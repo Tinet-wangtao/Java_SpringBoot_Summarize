183e84d3cd3d06c12c67e34ea77b9b67b0563bb8
Tinet-wangtao <wangtao@ti-net.com.cn>
2017-04-18 17:44:38 +0800

    添加SpringBoot的测试页面

---
+++ girl/src/main/java/com/wt/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.wt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author hohoTT
+ *
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String Hello() {
+		return "Hello TT!";
+	}
+
+}
